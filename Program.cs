a70c81c5f62fd87a07230e71ad5d821fbd6fdba9
seid1zahidov <saidiz@code.edu.az>
2022-04-12 14:30:40 -0700

    homejop

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            praduct praduct = new praduct("iphone");
+            praduct.Count = 17;
+            praduct.Price = 2900;
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            praduct.Sell();
+            Console.WriteLine($"{praduct.Count}\n{praduct.Totoalinciom}");
+        }
+    }
+}
---
+++ praduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework
+{
+    class praduct
+    {
+        public string  Name { get; set; }
+        public double Price { get; set; }
+        public int Count { get; set; }
+
+        public double Totoalinciom { get; set; }
+
+        public praduct(string name) 
+        {
+            Name = name;
+        
+        }
+        public void Sell() 
+        {
+            if (Count > 0)
+            {
+                Count--;
+                Totoalinciom += Price;
+            }
+            else
+            {
+                Console.WriteLine("mehsul bitdi");
+            }
+        
+        }
+
+
+
+
+
+    }
+}
